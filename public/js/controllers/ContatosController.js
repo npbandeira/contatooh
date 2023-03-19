82e66228d08024a6215bb408280d2e3aad2f40a8
Nicolas Pantoja <nicolaspantoja@hotmail.com>
2023-03-19 18:28:22 -0400

    Exibindo avisos para o usuário com as diretivas ng-show  ng-hide na Pagina Contatos

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -3,17 +3,22 @@ angular
   .controller("ContatosController", function ($scope, $resource) {
     // Contatos.html
     const Contato = $resource("/contatos/:id"); //$resouce devolve um objeto
+    $scope.contatos = []
 
     $scope.filtro = "";
 
+    $scope.mensagem ={texto:""}
+
+    // Request para buscar o contatos no banco
     function buscarContatos() {
       Contato.query(
         function (contatos) {
           $scope.contatos = contatos;
+          $scope.mensagem = {}
         },
         function (erro) {
-          console.log("Não foi possível obeter a lista de contatos");
           console.log(erro)
+          $scope.mensagem = {texto: "Não foi possível obter a lista de contatos"}
         }
       );
     }
@@ -23,7 +28,10 @@ angular
       Contato.delete({id: contato._id},
         buscarContatos,
         function(erro) {
-          console.log("Não foi possível remover o contato")
+          $scope.mensagem = {
+            texto: "Não foi possível remover o contato"
+          };
+
           console.log(erro)
         }
         )
